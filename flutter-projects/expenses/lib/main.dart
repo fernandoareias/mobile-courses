c2e33cdb8272042b474e99559f24a09d5b2fe1aa
f.areias <nando.calheirosx@gmail.com>
2023-09-09 13:30:15 -0300

    teste

---
+++ flutter-projects/expenses/lib/main.dart
@@ -0,0 +1,64 @@
+import 'package:expenses/models/transaction.model.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+    
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  
+  final _transaction = [
+    Transaction(id: "1", title:"teste", value: 230.76, date: DateTime.now()),
+    Transaction(id: "2", title:"teste 2", value: 231.76, date: DateTime.now())
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Column(
+        children: <Widget>[ 
+          Column(
+            children: _transaction.map((e) => Row(
+              children: [
+                Container(
+                  child: Text(e.value),
+                )
+              ],
+            )).toList(),
+          )
+        ]
+      ), 
+    );
+  }
+}
